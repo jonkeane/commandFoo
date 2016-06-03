30b31b84316017112ec67b0848d8aa2a67c4cc70
Jonathan Keane <jkeane@gmail.com>
2016-06-03 08:49:34 -0500

    extract ohare over 70 days

--- chicagoOnlygt70F.sh
+++ chicagoOnlygt70F.sh
@@ -3,3 +3,5 @@ Tfinc=21.111
 
 
 csvgrep -c 3 onlyOhare.csv -r 'TMAX' > tmax.csv
+awk -F,  '$4 > 21.111 {print $2}' tmax.csv > daysOver70
+csvgrep -c 2 -f daysOver70 onlyOhare.csv > ohareTempOver70f.csv
